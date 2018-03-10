368d2e4e0040eaf17e3361ada01443cfdddeaae7
vozturk <36339233+vozturk@users.noreply.github.com>
2018-03-10 17:50:33 +0300

    Update lab4.jl

--- lab4.jl
+++ lab4.jl
@@ -1,6 +1,25 @@
 using Knet
-include(Pkg.dir("Knet","data","mnist.jl"))#include mnist related functions
-xtrn,ytrn,xtst,ytst =mnist()#data ready to use in convolutional neural networks
+using Compat,GZip # Helpers to read the MNIST (Like lab-2)
+
+# We need to define preprocessing functions,e.g. downloading and loading the data
+function gzload(file; path="$file", url="http://yann.lecun.com/exdb/mnist/$file")
+    isfile(path) || download(url, path)
+    f = gzopen(path)
+    a = @compat read(f)
+    close(f)
+    return(a)
+end
+
+# That function is going to load raw data to the ram so that you will be able to use it
+function loaddata()
+    info("Loading MNIST...")
+    xtrn = gzload("train-images-idx3-ubyte.gz")[17:end]
+    xtst = gzload("t10k-images-idx3-ubyte.gz")[17:end]
+    ytrn = gzload("train-labels-idx1-ubyte.gz")[9:end]
+    ytst = gzload("t10k-labels-idx1-ubyte.gz")[9:end]
+    return (xtrn, ytrn, xtst, ytst)
+end
+
 
 function initweights(h)  # use cinit(x,h1,h2,...,hn,y) for n hidden layer model
     w = Any[]
@@ -84,6 +103,17 @@ LR        = 0.15;
 h   = ((28,28,1), (5,5,3), 10);
 
 srand(your_seed)
+
+xtrnraw, ytrnraw, xtstraw, ytstraw = loaddata(); # These are raw data and not suitable to direct usage
+# Following line takes the xtrnraw -> normalize, reshape and converts to Float32 data type
+xtrn = convert(Array{Float32}, reshape(xtrnraw ./ 255, 28, 28, 1, div(length(xtrnraw), 784)));
+xtst = convert(Array{Float32}, reshape(xtstraw ./ 255, 28, 28, 1, div(length(xtstraw), 784)));
+
+# We have 10 class classification 0 is represented as 10th class
+ytrnraw[ytrnraw.==0]=10; # That would be useful to understand that line
+ytrn = convert(Array{Float32}, sparse(convert(Vector{Int},ytrnraw),1:length(ytrnraw),one(eltype(ytrnraw)),10,length(ytrnraw)));
+ytstraw[ytstraw.==0]=10;
+ytst = convert(Array{Float32}, sparse(convert(Vector{Int},ytstraw),1:length(ytstraw),one(eltype(ytstraw)),10,length(ytstraw)));
 
 dtrn=minibatch(xtrn, ytrn, BATCHSIZE; xtype=KnetArray{Float32},ytype=KnetArray{Float32})
 dtst=minibatch(xtst, ytst, BATCHSIZE; xtype=KnetArray{Float32}, ytype=KnetArray{Float32})#returns list of tuples of (x,y)
