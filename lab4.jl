51e2646f013e929ba621b22f724bc354db6e5060
vozturk <36339233+vozturk@users.noreply.github.com>
2018-03-10 18:45:54 +0300

    Update lab4.jl

--- lab4.jl
+++ lab4.jl
@@ -83,7 +83,7 @@ function accuracy(weights, dtst, pred=cnn)
     for (x,y) in dtst
         ypred=pred(w,x)
         ypred[ypred.==maximum(ypred,1)] = 1
-        ypred[ypred.!=maximum(ypred,1)] = 0
+        ypred[ypred.!=1] = 0
         ncorrect+=sum(ypred.*y)
         nloss+= loss(w,x,y)
         ninstance += size(y,2)
